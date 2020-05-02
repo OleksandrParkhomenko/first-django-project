ea55426098dac8deb2c36a09a5b92676c8205b15
OleksandrParkhomenko <oleksandr.parhomenko@yahoo.com>
2020-05-02 18:03:48 +0300

    Save data from a Django form

--- django_first_project/django_first_project/views.py
+++ django_first_project/django_first_project/views.py
@@ -3,18 +3,28 @@ from django.shortcuts import render
 from django.template.loader import get_template
 
 
+from .forms import ContactForm
+
 def home_page(request):
-	title = "Hello there.."
+	title 	= "Hello there.."
 	context = {"title" : title}
 	return render(request, "home.html", context)
 
 def about_page(request):
-	title = "About us."
+	title 	= "About us."
 	return render(request, "helloworld.html", {"title" : title})
 
 def contact_page(request):
-	title = "Contact us."
-	return render(request, "helloworld.html", {"title" : title})
+	title 	= "Contact us."
+	form 	= ContactForm(request.POST or None)
+	if form.is_valid():
+		print(form.cleaned_data)
+		form = ContactForm()
+	context	= {
+		"title" : title,
+		"form" 	: form
+	}
+	return render(request, "form.html", context)
 
 
 def example_page(request):
